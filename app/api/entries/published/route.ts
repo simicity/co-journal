420e5596fcef005519dd556a65ebd1bf3a2c89f8
simicity <simi40nm@gmail.com>
2023-08-04 14:31:49 -0700

    Force to revalidate data fetching

--- app/api/entries/published/route.ts
+++ app/api/entries/published/route.ts
@@ -1,7 +1,16 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../lib/prisma";
 
+export const revalidate = 0;
+
 export async function GET() {
-  const entries = await prisma.entry.findMany({ where: { published: true } });
+  const entries = await prisma.entry.findMany({
+    where: {
+      published: true
+    },
+    orderBy: {
+      modifiedAt: "desc"
+    }
+  });
   return NextResponse.json(entries);
 }
