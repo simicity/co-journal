445c3e752d1db1cf7fb9aa05809466df6e812d57
simicity <simi40nm@gmail.com>
2023-08-03 18:59:22 -0700

    Add error check in APIs

--- app/api/entries/route.ts
+++ app/api/entries/route.ts
@@ -7,6 +7,9 @@ export async function POST(request: Request) {
   const session = await getServerSession(authOptions);
   const res = await request.json();
   const title = res.title;
+  if(!session?.user?.email) {
+    throw new Error("Invalid session");
+  }
   if(typeof title !== "string" || title.length === 0){
     throw new Error("Invalid title");
   }
@@ -18,13 +21,16 @@ export async function POST(request: Request) {
     title: title,
     content: content, 
     published: res.published ?? false,
-    author: { connect: { email: session?.user?.email } }
+    author: { connect: { email: session.user.email } }
   } });
   return NextResponse.json({});
 }
 
 export async function GET() {
   const session = await getServerSession(authOptions);
+  if(!session?.user?.email) {
+    throw new Error("Invalid session");
+  }
   const entries = await prisma.entry.findMany({ where: { author: session.user } });
   return NextResponse.json(entries);
 }
